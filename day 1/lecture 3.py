35758307c9ab89b9d338e1aaf277eeefb3b9d7c7
Basant <chbasantmalik@gmail.com>
2026-08-08 00:47:33 +0530

    python.learn

--- day 1/lecture 3.py
+++ day 1/lecture 3.py
@@ -2,4 +2,17 @@
 
 # print(input("what is your name "))
 
-print("hello "+input("what is your name ")+"!")
+# print("hello "+input("what is your name ")+"!")
+
+
+# variable
+
+# name = "jack"
+# print(name)
+# print(len(name))
+
+
+username = input("what is your name ")
+print(len(username))
+full_name = username +' Malik'
+print(full_name)
---
+++ day 1/lecture 4.py
@@ -0,0 +1,9 @@
+# Band Name Generator project
+
+print("welcome to the Band Name Generator.")
+city_name = input("what is the name of the city you grew up in?\n")
+# print(city_name)
+pet_name = input("what is the name of your pet?\n")
+# print(pet_name)
+band_name = city_name + " " + pet_name
+print("Your Band name could be " + band_name)
