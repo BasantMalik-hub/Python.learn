47cd9d2c079ce5a28314140d1908cfb29f82a62a
Basant <chbasantmalik@gmail.com>
2026-08-11 23:37:53 +0530

    python.learn

---
+++ 2.understanding datatype and how to manipulate strings/lecture1.py
@@ -0,0 +1,24 @@
+# python primitive data types
+
+# subscripting
+print("Hello"[0])
+# Method of pulling out a particular element from a string is called subscripting 
+# and number in b/w sq. brackets determines which character you're going to pull out
+
+# string
+print("123"+"345")
+# output of this will be 123345 and this additon of the strings is called concatenation
+
+# Integers = whole number
+print(123+456)
+
+# Large integers
+print(123_456_678)
+# python usually ignore "_" (DASH) and it primarily used for the developrer feasibility
+
+# Float = Floating Point Number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
