d2428a41579b9b26f2d7f9f72660a48040797601
Basant <chbasantmalik@gmail.com>
2026-08-16 23:21:32 +0530

    python.learn

---
+++ 2.understanding datatype and how to manipulate strings/number_manipulation.py
@@ -0,0 +1,31 @@
+bmi = 84/1.65**2
+print(bmi)
+# 30.85399449035813
+
+print(int(bmi))
+# 30
+
+print(round(bmi))
+# 31
+
+# round(number,ndigits)
+
+print(round(bmi,2))
+# 30.85
+
+# Assignment operators
+
+score=0
+
+score+=1
+print(score)
+
+score-=1
+print(score)
+
+score*=1
+print(score)
+
+score/=1
+print(score)
+
---
+++ 2.understanding datatype and how to manipulate strings/type_error_type_checking_type_conversion.py
@@ -0,0 +1,24 @@
+# type checking
+
+# integer
+print(type(123))
+# string
+print(type("123"))
+# large integer
+print(type(123_456_678))
+# float
+print(type(3.14159))
+# Boolean
+print(type(True))
+print(type(False))
+
+
+# type conversion - converting piece of data into different data type
+
+# integer to boolean
+print(bool(1))
+print(bool(0))
+
+
+print("no. of letter in your name : "+ str(len(input("enter your name ").replace(" ",""))))
+
